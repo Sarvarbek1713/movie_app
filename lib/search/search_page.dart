1e4d497d94fc350e6cf40f7bdab46a05aed2dee5
Sarvarbek1713 <sarvarbekkamoliddinov1303>
2024-06-09 13:11:23 +0500

    111

--- lib/search/search_page.dart
+++ lib/search/search_page.dart
@@ -2,26 +2,24 @@ import 'package:flutter/material.dart';
 import 'package:flutter_svg/svg.dart';
 import 'package:movie_app/h%20(2).dart';
 import 'package:movie_app/home/home_page.dart';
-import 'package:movie_app/home/wishlist.dart';
 import 'package:movie_app/movie_interface.dart/movie_page.dart';
 
 class SearchPage extends StatefulWidget with ScreenSizeUtil {
   const SearchPage({super.key});
 
   @override
-  // ignore: library_private_types_in_public_api
   _SearchPageState createState() => _SearchPageState();
 }
 
 class _SearchPageState extends State<SearchPage> with ScreenSizeUtil {
   final TextEditingController _searchController = TextEditingController();
-  List<String> _movies = [
-    'Spider-Man No Way Home',
-    'The Batman',
-    'Doctor Strange',
-    'Thor: Love and Thunder'
+  final List<Map<String, String>> _movies = [
+    {'title': 'Spider-Man No Way Home', 'date': '2021', 'time': '148 minutes'},
+    {'title': 'The Batman', 'date': '2022', 'time': '176 minutes'},
+    {'title': 'Doctor Strange', 'date': '2016', 'time': '115 minutes'},
+    {'title': 'Thor: Love and Thunder', 'date': '2022', 'time': '119 minutes'},
   ];
-  List<String> _filteredMovies = [];
+  List<Map<String, String>> _filteredMovies = [];
 
   @override
   void initState() {
@@ -34,7 +32,7 @@ class _SearchPageState extends State<SearchPage> with ScreenSizeUtil {
     final query = _searchController.text.toLowerCase();
     setState(() {
       _filteredMovies = _movies
-          .where((movie) => movie.toLowerCase().contains(query))
+          .where((movie) => movie['title']!.toLowerCase().contains(query))
           .toList();
     });
   }
@@ -56,23 +54,23 @@ class _SearchPageState extends State<SearchPage> with ScreenSizeUtil {
           children: [
             Padding(
               padding: EdgeInsets.symmetric(
-                      horizontal: getWidth(context) * 0.06,
-                      vertical: getHeight(context) * 0.05)
+                      horizontal: 24, vertical: getHeight(context) * 0.05)
                   .copyWith(bottom: getHeight(context) * 0.03),
-              child: SearchiBar(
-                controller: _searchController,
-              ),
+              child: SearchiBar(controller: _searchController),
             ),
-            const CategorySearchWidget(),
             Padding(
-              padding: EdgeInsets.symmetric(
-                horizontal: getHeight(context) * 0.03,
-              ).copyWith(top: 8),
+              padding:
+                  EdgeInsets.symmetric(horizontal: getHeight(context) * 0.03)
+                      .copyWith(top: 0),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
                 crossAxisAlignment: CrossAxisAlignment.start,
                 children: _filteredMovies
-                    .map((movie) => MovieCard(title: movie))
+                    .map((movie) => MovieCard(
+                          title: movie['title']!,
+                          date: movie['date']!,
+                          time: movie['time']!,
+                        ))
                     .toList(),
               ),
             ),
@@ -85,152 +83,160 @@ class _SearchPageState extends State<SearchPage> with ScreenSizeUtil {
 
 class MovieCard extends StatelessWidget with ScreenSizeUtil {
   final String title;
-  const MovieCard({super.key, required this.title});
+  final String date;
+  final String time;
+
+  const MovieCard({
+    super.key,
+    required this.title,
+    required this.date,
+    required this.time,
+  });
 
   @override
   Widget build(BuildContext context) {
-    final favoritesManager = FavoritesManager();
-
-    return InkWell(
-      onTap: () {
-        Navigator.push(
-          context,
-          MaterialPageRoute(
-            builder: (context) => const MoviePage(),
-          ),
-        );
-      },
-      child: Row(
-        children: [
-          Container(
-            decoration: BoxDecoration(
-              borderRadius: BorderRadius.circular(8),
+    return Padding(
+      padding: const EdgeInsets.only(bottom: 10),
+      child: InkWell(
+        onTap: () {
+          Navigator.push(
+            context,
+            MaterialPageRoute(
+              builder: (context) => const MoviePage(),
             ),
-            child: Image.asset(
-              'assets/images/spdrmn.png',
-              width: getHeight(context) * 0.18,
+          );
+        },
+        child: Row(
+          children: [
+            Container(
+              decoration: BoxDecoration(
+                borderRadius: BorderRadius.circular(8),
+              ),
+              child: Image.asset(
+                'assets/images/spdrmn.png',
+                width: getHeight(context) * 0.18,
+              ),
             ),
-          ),
-          Padding(
-            padding: const EdgeInsets.symmetric(horizontal: 16),
-            child: Column(
-              mainAxisSize: MainAxisSize.min,
-              mainAxisAlignment: MainAxisAlignment.start,
-              crossAxisAlignment: CrossAxisAlignment.start,
-              children: [
-                Container(
-                  padding:
-                      const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
-                  decoration: BoxDecoration(
-                    borderRadius: BorderRadius.circular(6),
-                    color: const Color(0xffFF8700),
-                  ),
-                  child: const Text(
-                    'Premium',
-                    style: TextStyle(
+            Padding(
+              padding: const EdgeInsets.symmetric(horizontal: 16),
+              child: Column(
+                mainAxisSize: MainAxisSize.min,
+                mainAxisAlignment: MainAxisAlignment.start,
+                crossAxisAlignment: CrossAxisAlignment.start,
+                children: [
+                  Container(
+                    padding:
+                        const EdgeInsets.symmetric(horizontal: 8, vertical: 4),
+                    decoration: BoxDecoration(
+                      borderRadius: BorderRadius.circular(6),
+                      color: const Color(0xffFF8700),
+                    ),
+                    child: const Text(
+                      'Premium',
+                      style: TextStyle(
                         fontSize: 12,
                         color: Colors.white,
-                        fontWeight: FontWeight.w500),
+                        fontWeight: FontWeight.w500,
+                      ),
+                    ),
                   ),
-                ),
-                Padding(
-                  padding:
-                      EdgeInsets.symmetric(vertical: getHeight(context) * 0.01),
-                  child: Text(
-                    title,
-                    maxLines: 1,
-                    textAlign: TextAlign.left,
-                    overflow: TextOverflow.ellipsis,
-                    style: const TextStyle(
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        vertical: getHeight(context) * 0.01),
+                    child: Text(
+                      title,
+                      maxLines: 1,
+                      textAlign: TextAlign.left,
+                      style: const TextStyle(
                         fontSize: 16,
                         color: Colors.white,
-                        fontWeight: FontWeight.w600),
+                        fontWeight: FontWeight.w600,
+                      ),
+                    ),
                   ),
-                ),
-                const Row(
-                  children: [
-                    Icon(
-                      Icons.calendar_month,
-                      size: 14,
-                      color: Colors.grey,
-                    ),
-                    SizedBox(
-                      width: 8,
-                    ),
-                    Text(
-                      textAlign: TextAlign.left,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                      '2021. ',
-                      style: TextStyle(
-                          fontSize: 18,
-                          color: Colors.grey,
-                          fontWeight: FontWeight.w600),
-                    ),
-                  ],
-                ),
-                Padding(
-                  padding:
-                      EdgeInsets.symmetric(vertical: getHeight(context) * 0.01),
-                  child: const Row(
+                  Row(
                     children: [
-                      Icon(
-                        Icons.timelapse,
-                        size: 18,
+                      const Icon(
+                        Icons.calendar_month,
+                        size: 14,
                         color: Colors.grey,
                       ),
-                      SizedBox(
-                        width: 8,
-                      ),
+                      const SizedBox(width: 8),
                       Text(
+                        date,
                         textAlign: TextAlign.left,
                         overflow: TextOverflow.ellipsis,
                         maxLines: 1,
-                        '148 minutes. ',
-                        style: TextStyle(
-                            fontSize: 16,
-                            color: Colors.grey,
-                            fontWeight: FontWeight.w600),
+                        style: const TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.w600,
+                        ),
                       ),
                     ],
                   ),
-                ),
-                const Row(
-                  children: [
-                    Icon(
-                      Icons.local_movies,
-                      size: 18,
-                      color: Colors.grey,
-                    ),
-                    SizedBox(
-                      width: 8,
-                    ),
-                    Text(
-                      textAlign: TextAlign.left,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                      'Action | ',
-                      style: TextStyle(
-                          fontSize: 16,
+                  Padding(
+                    padding: EdgeInsets.symmetric(
+                        vertical: getHeight(context) * 0.01),
+                    child: Row(
+                      children: [
+                        const Icon(
+                          Icons.timelapse,
+                          size: 18,
                           color: Colors.grey,
-                          fontWeight: FontWeight.w600),
+                        ),
+                        const SizedBox(width: 8),
+                        Text(
+                          time,
+                          textAlign: TextAlign.left,
+                          overflow: TextOverflow.ellipsis,
+                          maxLines: 1,
+                          style: const TextStyle(
+                            fontSize: 14,
+                            color: Colors.grey,
+                            fontWeight: FontWeight.w600,
+                          ),
+                        ),
+                      ],
                     ),
-                    Text(
-                      textAlign: TextAlign.left,
-                      overflow: TextOverflow.ellipsis,
-                      maxLines: 1,
-                      'Movie',
-                      style: TextStyle(
-                          fontSize: 16,
+                  ),
+                  const Row(
+                    children: [
+                      Icon(
+                        Icons.local_movies,
+                        size: 18,
+                        color: Colors.grey,
+                      ),
+                      SizedBox(width: 8),
+                      Text(
+                        'Action | ',
+                        textAlign: TextAlign.left,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: TextStyle(
+                          fontSize: 14,
+                          color: Colors.grey,
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                      Text(
+                        'Фильм',
+                        textAlign: TextAlign.left,
+                        overflow: TextOverflow.ellipsis,
+                        maxLines: 1,
+                        style: TextStyle(
+                          fontSize: 14,
                           color: Colors.white,
-                          fontWeight: FontWeight.w600),
-                    ),
-                  ],
-                ),
-              ],
+                          fontWeight: FontWeight.w600,
+                        ),
+                      ),
+                    ],
+                  ),
+                ],
+              ),
             ),
-          ),
-        ],
+          ],
+        ),
       ),
     );
   }
